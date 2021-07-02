d5994d7c979e86d68126927879fa68cd0b437125
Saad Najmi <sanajmi@microsoft.com>
2021-07-02 15:21:22 -0500

    Simplify one file a bit

--- packages/experimental/ContextualMenuMacOS/src/ContextualMenu.tsx
+++ packages/experimental/ContextualMenuMacOS/src/ContextualMenu.tsx
@@ -4,19 +4,17 @@ import { View } from 'react-native';
 import {
   contextualMenuName,
   ContextualMenuProps,
-  ContextualMenuSlotProps,
+  // ContextualMenuSlotProps,
   ContextualMenuType,
-  ContextualMenuRenderData,
+  // ContextualMenuRenderData,
   ContextualMenuContext,
-  ContextualMenuState,
+  // ContextualMenuState,
 } from './ContextualMenu.types';
-import { settings } from './ContextualMenu.settings';
-import { IUseComposeStyling, compose } from '@uifabricshared/foundation-compose';
-import { useSelectedKey } from '@fluentui-react-native/interactive-hooks';
-import { mergeSettings } from '@uifabricshared/foundation-settings';
-import { backgroundColorTokens, borderTokens } from '@fluentui-react-native/tokens';
+// import { settings } from './ContextualMenu.settings';
+import { compose, withSlots, UseSlots, buildUseStyling, mergeProps } from '@fluentui-react-native/framework';
+// import { useSelectedKey } from '@fluentui-react-native/interactive-hooks';
+// import { backgroundColorTokens, borderTokens } from '@fluentui-react-native/tokens';
 import { Callout } from '@fluentui-react-native/callout';
-import { ISlots, withSlots } from '@uifabricshared/foundation-composable';
 
 export const CMContext = React.createContext<ContextualMenuContext>({
   selectedKey: null,
@@ -30,66 +28,25 @@ export const CMContext = React.createContext<ContextualMenuContext>({
 
 export const ContextualMenu = compose<ContextualMenuType>({
   displayName: contextualMenuName,
-  usePrepareProps: (userProps: ContextualMenuProps, useStyling: IUseComposeStyling<ContextualMenuType>) => {
-    const { setShowMenu, shouldFocusOnMount = true, shouldFocusOnContainer = false, ...rest } = userProps;
-
-    // This hook updates the Selected Button and calls the customer's onClick function. This gets called after a button is pressed.
-    const data = useSelectedKey(null, userProps.onItemClick);
-
-    const dismissCallback = React.useCallback(() => {
-      userProps.onDismiss();
-      setShowMenu(false);
-    }, [setShowMenu, userProps.onDismiss]);
-
-    const [containerFocus, setContainerFocus] = React.useState(true);
-    const toggleContainerFocus = React.useCallback(() => {
-      setContainerFocus(false);
-    }, [setContainerFocus]);
-
-    const state: ContextualMenuState = {
-      context: {
-        selectedKey: data.selectedKey,
-        onItemClick: data.onKeySelect,
-        onDismissMenu: dismissCallback,
-      },
-    };
-
-    const styleProps = useStyling(userProps, (override: string) => state[override] || userProps[override]);
-
-    const slotProps = mergeSettings<ContextualMenuSlotProps>(styleProps, {
-      root: {
-        ...rest,
-        setInitialFocus: shouldFocusOnMount,
-      },
-      container: {
-        accessible: shouldFocusOnContainer,
-        focusable: shouldFocusOnContainer && containerFocus,
-        onBlur: toggleContainerFocus,
-      },
-    });
-
-    return { slotProps, state };
-  },
-  settings: settings,
+  tokens: [],
+  tokensThatAreAlsoProps: [],
   slots: {
     root: Callout,
     container: View,
   },
-  styles: {
-    root: [backgroundColorTokens, borderTokens],
-    container: [],
+  slotProps: {
+    root: {},
+    container: {},
   },
-  render: (Slots: ISlots<ContextualMenuSlotProps>, renderData: ContextualMenuRenderData, ...children: React.ReactNode[]) => {
-    if (renderData.state == undefined) {
-      return null;
-    }
-    return (
-      <CMContext.Provider value={renderData.state.context}>
-        <Slots.root>
+  render: (props: ContextualMenuProps, useSlots: UseSlots<ContextualMenuType>) => {
+    const Slots = useSlots(props);
+    return (rest: ContextualMenuProps, children: React.ReactNode[]) => {
+      return (
+        <Slots.root {...mergeProps(props, rest)}>
           <Slots.container>{children}</Slots.container>
         </Slots.root>
-      </CMContext.Provider>
-    );
+      );
+    };
   },
 });
 
